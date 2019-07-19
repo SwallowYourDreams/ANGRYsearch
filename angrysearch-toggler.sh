39280e066b89fbd03841883e6a6d282214f40a51
ParaDice13 <52176040+ParaDice13@users.noreply.github.com>
2019-07-19 17:09:48 +0200

    Update angrysearch-toggler.sh

--- angrysearch-toggler.sh
+++ angrysearch-toggler.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Toggles ANGRYsearch, i.e. opens ANGRYsearch if it's closed and closes it if it's open.
+# Tie this script to a key combination (e.g. Ctrl + Space) to access ANGRYsearch more quickly
 pid=$(pgrep '^angrysearch$')
 if [ -z $pid ] ; then
 	nohup angrysearch > /dev/null &
